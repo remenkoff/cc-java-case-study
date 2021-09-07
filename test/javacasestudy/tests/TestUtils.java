f82728f5d48beefca3f052cef8ac91922bd70ac4
Ruslan Remenkov <remenkoff@gmail.com>
2021-09-08 02:20:24 +0300

    Group the tests into a Nested sets

--- test/javacasestudy/tests/TestUtils.java
+++ test/javacasestudy/tests/TestUtils.java
@@ -3,7 +3,16 @@ package javacasestudy.tests;
 import java.util.Random;
 
 public class TestUtils {
-  public static int randomInt(int bound) {
+  public static int randomIntFromZeroTo(int bound) {
     return (new Random()).nextInt(bound);
+  }
+
+  public static String randomStringLowercased() {
+    int aLetter = 97;
+    int bLetter = 122;
+    return (new Random()).ints(aLetter, bLetter + 1)
+      .limit(32)
+      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+      .toString();
   }
 }
